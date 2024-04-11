cd4d6253bc47cdfdef914d55e1b68252c9039693
Naomi Berhane <naomi.berhane93@gmail.com>
2024-04-11 03:16:53 +0200

    Correct quotationmarks in mult_choices tuple

--- run.py
+++ run.py
@@ -18,16 +18,16 @@ food_questions = (
 food_questions_number = 0
 
 mult_choices = (
-    ("1. Japan”, “2. Vietnam”, “3. Thailand”, “4. China"),
-    ("1. Rice”, “2. Potatoes”, “3. Noodles”, “4. Tofu"),
-    ("1. Sushi”, “2. Tacos”, “3. Ceviche”, “4. Hamburger"),
-    ("1. Greece”, “2. Turkey”, “3. Egypt”, “4. Marocco"),
-    ("1. Fondue”, “2. Kimchi”, “3. Pizza”, “4. Sushi"),
-    ("1. Baguette”, “2. Roti”, “3. Jerk chicken”, “4. Ackee and saltfish"),
-    ("1. Poland”, “2. Hungary”, “3. Czech Republic”, “4. Slovakia"),
-    ("1. Empanadas”, “2. Sushi”, “3. Pho”, “4. Tacos"),
-    ("1. Pad Thai”, “2. Feijoada”, “3. Kimchi”, “4. Gyros"),
-    ("1. Russia”, “2. France”, “3. Italy”, “4. Spain")  
+    ("1. Japan", "2. Vietnam", "3. Thailand", "4. China"),
+    ("1. Rice", "2. Potatoes", "3. Noodles", "4. Tofu"),
+    ("1. Sushi", "2. Tacos", "3. Ceviche", "4. Hamburger"),
+    ("1. Greece", "2. Turkey", "3. Egypt", "4. Marocco"),
+    ("1. Fondue", "2. Kimchi", "3. Pizza", "4. Sushi"),
+    ("1. Baguette", "2. Roti", "3. Jerk chicken", "4. Ackee and saltfish"),
+    ("1. Poland", "2. Hungary", "3. Czech Republic", "4. Slovakia"),
+    ("1. Empanadas", "2. Sushi", "3. Pho", "4. Tacos"),
+    ("1. Pad Thai", "2. Feijoada", "3. Kimchi", "4. Gyros"),
+    ("1. Russia", "2. France", "3. Italy", "4. Spain")  
 )
 
 correct_mult_choice = ("2", "2", "3", "1", "1", "4", "1", "1", "2", "1")
@@ -62,5 +62,6 @@ def conduct_quiz(food_questions, mult_choices, correct_mult_choice):
                 print("Invalid answer! Please choose between numbers 1, 2, 3 or 4")
 
     print("\nQuiz completed!\nHere are your answers:")
+
 conduct_quiz(food_questions, mult_choices, correct_mult_choice)
 
